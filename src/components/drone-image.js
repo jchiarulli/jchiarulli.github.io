0e3e20e45a4596028f576a83f9d8832895ea790b
Jason Chiarulli <jchiarulli@protonmail.com>
2020-11-05 00:42:06 -0500

    Started the Projects section on the homepage

---
+++ src/components/drone-image.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+const DroneImage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      drone: file(relativePath: { eq: "drone.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 350, maxHeight: 250, quality: 100) {
+            ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluidLimitPresentationSize
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <Img
+      fluid={data.drone.childImageSharp.fluid}
+      style={{ display: "flex", flex: "1" }}
+    />
+  );
+};
+
+export default DroneImage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import Image from "../components/image";
+import DroneImage from "../components/drone-image";
 import SEO from "../components/seo";
 import classes from "./index.module.less";
 
 const IndexPage = (props) => (
   <Layout location={props.location}>
     <SEO title="Home" />
-    <div className={classes.header}>
+    <div className={classes.about_me_header}>
       <h2>About Me</h2>
     </div>
     <div className={classes.profile_pic_and_about_me}>
@@ -20,6 +21,20 @@ const IndexPage = (props) => (
           Drones, and anything else that peeks my interest.
         </p>
       </div>
+    </div>
+    <div className={classes.projects_header}>
+      <h2>Projects</h2>
+    </div>
+    <h3 className={classes.project_titles}>Object Detection Drone</h3>
+    <div className={classes.projects}>
+      <DroneImage />
+    </div>
+    <div className={classes.drone_project}>
+      <p>
+        Here's a drone I built & calibrated using the Navio2, Python, and
+        APMPlanner2. It's capable of streaming video using ImageZMQ, and
+        detecting people using OpenCV's deep learning models.
+      </p>
     </div>
   </Layout>
 );
