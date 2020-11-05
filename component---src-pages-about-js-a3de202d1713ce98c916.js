11b16c31ee490dc63e65fde704d93c4c17a47d63
Jason Chiarulli <jchiarulli@protonmail.com>
2020-11-04 23:42:29 -0500

    Updates

---
+++ component---src-pages-about-js-a3de202d1713ce98c916.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"3XHS":function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),l=a.n(t),r=a("Bl7J"),u=a("vrFN");n.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"About"}),l.a.createElement("div",null,l.a.createElement("h1",null,"Hey everyone!"),l.a.createElement("p",null,"My name is Jason Chiarulli."),l.a.createElement("p",null,"I enjoy learning about JavaScript & Python.")))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-a3de202d1713ce98c916.js.map
